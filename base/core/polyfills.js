2c7b77e4e30db3e849d386f29b3d9ee2a0ad42e3
rokyed <rokyed@gmail.com>
2015-11-18 09:24:07 -0500

    [adding polyfills]

---
+++ base/core/polyfills.js
@@ -0,0 +1,11 @@
+// custom event for ie
+(function () {
+  function CustomEvent (event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+    return evt;
+   };
+  CustomEvent.prototype = window.CustomEvent.prototype;
+  window.CustomEvent = CustomEvent;
+})();
